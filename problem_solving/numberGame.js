380fb61b759a788f83b6b9245efb38b8e1d37108
meganathan <meganathan212003@gmail.com>
2024-03-11 22:11:40 +0530

    solve new problem

--- problem_solving/numberGame.js
+++ problem_solving/numberGame.js
@@ -5,7 +5,16 @@
  * @author meganathan
  */
 
-let numberGame = function (nums) {
-
-    
+var numberGame = function (nums) {
+    let numb = nums.sort((a, b) => b - a)
+    console.log(numb);
+    let arr = []
+    while (numb.length) {
+        let alice = numb.pop()
+        let bob = numb.pop()
+        arr.push(bob, alice)
+    }
+    return arr
 }
+
+console.log(numberGame([5, 4, 2, 3])); // Output: [3, 2, 5, 4]
